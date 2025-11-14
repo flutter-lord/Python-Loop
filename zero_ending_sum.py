a2b51d21e4da665e54effbb997bdb2be01655028
flutter-lord <richardolojede2@gmail.com>
2025-11-14 19:26:37 +0100

    commit -m

--- zero_ending_sum.py
+++ zero_ending_sum.py
@@ -1,8 +1,10 @@
-numbers = eval(input("Enter any digits, the sum end when you input 0: "))
+#numbers = eval(input("Enter any digits, the sum end when you input 0: "))
+import numbers
+
 summation = 0
 
 while numbers != 0:
     summation += numbers
-    numbers = eval(input("continue inputting any integer: "))
+    #numbers = eval(input("continue inputting any integer: "))
 
 print(summation)
