f65d00840abd250cf6dc3f7e8b4596a16a4a992e
flutter-lord <richardolojede2@gmail.com>
2025-11-15 10:51:10 +0100

    commit -m

--- future_tuition.py
+++ future_tuition.py
@@ -1,9 +1,28 @@
-balance = 1000
-while True:
-    if balance == 9 :
-        break
-    balance -= 9
+NUMBERS_OF_PRIMES = eval(input("Enter the number of prime numbers you want to see: "))
+NUMBERS_OF_PRIMES_PER_LINES = 10
 
-print("Balance: = ", balance)
+count = 0
+number = 2
+
+while count < NUMBERS_OF_PRIMES:
+    isPrime = True
+
+    divisor = 2
+
+    while divisor <= number // 2:
+        if number % divisor == 0:
+            isPrime = False
+            break
+        divisor += 1
+
+    if isPrime:
+        count += 1
+
+        print(format(number, "5d"), end = "")
+
+        if count % NUMBERS_OF_PRIMES_PER_LINES == 0:
+            print()
+
+    number += 1
 
 
