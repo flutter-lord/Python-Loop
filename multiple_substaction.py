1aff25d19bf151f0ca899b638f4f9f67dbb607c4
flutter-lord <richardolojede2@gmail.com>
2025-11-22 22:06:58 +0100

    commit -m

--- multiple_substaction.py
+++ multiple_substaction.py
@@ -1,10 +1,14 @@
 import random
-import sys
 import time
 
 correct_count = 0
 incorrect_count = 0
 count = 0
+
+lateef1 = random.randint(1, 100)
+lateef2 = random.randint(1, 100)
+
+print(lateef1, lateef2)
 NUMBER_OF_QUESTIONS = eval(input("How many questions do you want to answer?: "))
 
 start_time = time.time()
@@ -19,11 +23,12 @@ while count < NUMBER_OF_QUESTIONS:
     answer = eval(input("What is"+" "+ str(num1) + " - " + str(num2) + ": "))
 
     if (num1 - num2) == answer:
-        print("Yah Correct!")
+        print("Wow, Your answers",answer,"is Correct!")
         correct_count += 1
 
     else:
-        print("Yah Incorrect!")
+        print("No,Your answer",answer,"is Incorrect!",)
+        print("Your correct answer should be:",(num1 - num2))
         incorrect_count += 1
 
     count += 1
@@ -35,36 +40,3 @@ test_time  = int(end_time - start_time)
 print(correct_count,"answers in",NUMBER_OF_QUESTIONS,"questions is correct")
 print(incorrect_count,"answers in",NUMBER_OF_QUESTIONS,"questions is incorrect ")
 print("\nYou spent", test_time, "secs for the test")
-
-
-count = 0
-correct_count = 0
-incorrect_count = 0
-question_another = input("Do you want to answer another question, Enter y for yes ann n for no: ")
-
-while  question_another == 'y':
-      NUMBER_OF_QUESTIONS = eval(input("How many questions do you want to answer?: "))
-      while count < NUMBER_OF_QUESTIONS:
-          num1 = random.randint(0, 9)
-          num2 = random.randint(0, 9)
-
-          if num1 < num2:
-              num1, num2 = num2, num1
-
-          answer = eval(input("What is" + " " + str(num1) + " - " + str(num2) + ": "))
-
-          if (num1 - num2) == answer:
-              print("Yah Correct!")
-              correct_count += 1
-
-          else:
-              print("Yah Incorrect!")
-              incorrect_count += 1
-
-          count += 1
-
-print(correct_count, "answers in", NUMBER_OF_QUESTIONS, "questions is correct")
-print(incorrect_count, "answers in", NUMBER_OF_QUESTIONS, "questions is incorrect ")
-print("\nYou spent", test_time, "secs for the test")
-
-sys.exit()
