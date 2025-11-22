471c5fc2850726657ba1cd5ae100d3aa32691e45
flutter-lord <richardolojede2@gmail.com>
2025-11-23 00:07:47 +0100

    commit -m

--- multiple_substaction.py
+++ multiple_substaction.py
@@ -1,14 +1,9 @@
 import random
+import sys
 import time
-
 correct_count = 0
 incorrect_count = 0
 count = 0
-
-lateef1 = random.randint(1, 100)
-lateef2 = random.randint(1, 100)
-
-print(lateef1, lateef2)
 NUMBER_OF_QUESTIONS = eval(input("How many questions do you want to answer?: "))
 
 start_time = time.time()
@@ -31,7 +26,13 @@ while count < NUMBER_OF_QUESTIONS:
         print("Your correct answer should be:",(num1 - num2))
         incorrect_count += 1
 
+    continue_question = input("Would you like to continue the question? (y/n): ")
+    while continue_question == "n" or continue_question == "N":
+        print("This is the end of your test")
+        sys.exit()
     count += 1
+
+
 
 end_time = time.time()
 
@@ -39,4 +40,4 @@ test_time  = int(end_time - start_time)
 
 print(correct_count,"answers in",NUMBER_OF_QUESTIONS,"questions is correct")
 print(incorrect_count,"answers in",NUMBER_OF_QUESTIONS,"questions is incorrect ")
-print("\nYou spent", test_time, "secs for the test")
+print("\nYou spent", test_time, "secs for the test\n")
