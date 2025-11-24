d80352830161622d82bcd8529655cf9e99b70489
flutter-lord <richardolojede2@gmail.com>
2025-11-24 23:45:25 +0100

    commit -m

--- positive_negative.py
+++ positive_negative.py
@@ -1,10 +1,11 @@
-negative_count = 0
-positive_count = 0
+negative_count, positive_count, average, total_numbers = 0, 0, 0 , 0
 
 integers = eval(input("Enter an integer, the input ends if its is 0: "))
+add_up = integers
 
 while integers != 0:
     integers = eval(input("Enter an integer, the input ends if its is 0: "))
+    add_up += integers
 
     if integers < 0:
         negative_count += 1
@@ -12,6 +13,11 @@ while integers != 0:
     else:
         positive_count += 1
 
-print("The number of positive integers is:", positive_count,"\nThe number of negative integers is:", negative_count)
+    total_numbers = positive_count + negative_count
+    average = add_up / total_numbers
 
+print("The number of positive integers is:", positive_count,"\nThe number of negative integers is:", negative_count)
+print("The average is:", average)
+print("The sum of all the numbers is:", add_up)
+print("Total number is:", total_numbers)
 
